2426a8beafc81322fa6e1ec6dea6abc693324fe6
Jim Barnes <jimbarnesdeveloper@gmail.com>
2023-02-07 13:50:15 -0500

    Found and replaced template blocks from the initial creation of the repo

---
+++ ucf-online-entries-exporter.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: UCF Online Entries Exporter
+Description: Provides a WP CLI command for exporting gravity form entries to an external MySQL database.
+Version: 1.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/UCF-Online-Entries-Exporter
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
